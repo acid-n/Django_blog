2b0760912c3fabcb9d154822d2e78a07a2281394
Nikolay <acid_n@mail.ru>
2020-07-05 15:57:59 +0300

    Update my blog story

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -18,6 +24,12 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()  # Менеджер по умолчанию
+    published = PublishedManager()  # Наш новы менеджер
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
 
 
 class Meta:
